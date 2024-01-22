0bbd2d9bd5f232d30ac3e6d5b1b7162a37e70032
purushotham <purushothamgowda847@gmail.com>
2024-01-22 20:27:05 +0530

    sl

--- src/compounents/projects.jsx
+++ src/compounents/projects.jsx
@@ -10,7 +10,7 @@ export default  function projects(){
 
 <h1 class="font-bold  text-center   text-4xl text-black ">Projects</h1>
 <div className='grid grid-cols-1 lg:grid-cols-1 xl:grid-cols-2 2xl:grid-cols-3 '>
-<Comp img={'weather.png'}  title={'WEATHER APP'} des={"Weather app is weather forecasting application built using javascript.in this project Api is handled , data of all around the world is avaliable."} demo={ "https://weather-pq82t6eox-purushotham847.vercel.app/ "}  code={"https://github.com/purushotham847/weather_app" }/>
+<Comp img={'weather.png'}  title={'WEATHER APP'} des={"Weather app is weather forecasting application built using javascript.in this project Api is handled , data of all around the world is avaliable."} demo={ "https://purushotham847.github.io/weather_app/ "}  code={"https://github.com/purushotham847/weather_app" }/>
 
 </div>
 <div>
